4d8fbb32c9534c1bd7c37c9f997f5763dd5148d2
= <kzmudz79@gmail.com>
2020-03-12 14:43:27 -0700

    Updated to work with unity render

--- solver.cpp
+++ solver.cpp
@@ -1,13 +1,17 @@
 #include "grid_fns.h"
 #include "utils.h"
 #include "advect.h"
+#include <string>
 #include <stdlib.h>
 #include <iostream>
 
 int main(int argc, char* argv[]){
 /* Below is basic skeleton of a fluid solver. Each function will be implemented,
 and combine to give us a simulator.*/
-	int numFrames = 60;
+	srand(3);
+
+	string fileName = "outputVelocities.txt";
+	int numFrames = 300;
 	int xDim = 32;
 	int yDim = 32;
 
@@ -20,6 +24,9 @@ and combine to give us a simulator.*/
 		xDim = atoi(argv[2]);
 		yDim = atoi(argv[3]);
 	}
+	// Make sure no existing data already in save destination.
+	clearOutputFile(fileName);
+
 	// 1. Initialize grids with fluid
 	vector< vector<float> > pressureGrid(xDim, vector<float>(yDim, initValue));
 	vector< vector<float> > horizVelocityGrid(xDim+1, vector<float>(yDim, initValue));
@@ -29,26 +36,28 @@ and combine to give us a simulator.*/
 	vector< vector<float> > updatedHorizGrid(xDim+1, vector<float>(yDim, initValue));
 	vector< vector<float> > updatedVertGrid(xDim, vector<float>(yDim+1, initValue));
 
-	/*
-	Used for creating timestep, eventually, don't need for right now
 
+	// Used for creating timestep, eventually, don't need for right now
 	vector<float>* bottomLeftVel = rightSideVel(horizVelocityGrid, vertVelocityGrid, 0, 0);
 	float maxVel = magnitude(*bottomLeftVel);
 	delete bottomLeftVel;
-	*/
+
 
 	const float TIME_PER_FRAME = 1 / 15.0;
 	for (int i = 0; i < numFrames; ++i) {
 		t = 0;
 		while (t < TIME_PER_FRAME) {
 			deltaT = 1 / maxVel;
-
+			saveVelocityField(horizVelocityGrid, vertVelocityGrid, xDim, yDim, fileName);
 			advect(horizVelocityGrid, vertVelocityGrid, updatedHorizGrid, updatedVertGrid, xDim, yDim, deltaT);
+
+			horizVelocityGrid = updatedHorizGrid;
+			vertVelocityGrid = updatedVertGrid;
 	 		// pressure Projection
 	 		// advect free surface
 			t = t + deltaT;
 		}
 	// 	save frame i
 	}
-
+	saveVelocityField(horizVelocityGrid, vertVelocityGrid, xDim, yDim, fileName);
 }
--- utils.h
+++ utils.h
@@ -2,7 +2,71 @@
 #define __UTILS__
 
 #include <cmath>
+#include <fstream>
+#include <sstream>
 using namespace std;
+
+/*
+	Return type: void
+*/
+void clearOutputFile(string fileName) {
+	/*
+	Opens and clears the given fileName for later use.
+	*/
+	ofstream outputFile;
+	outputFile.open(fileName, ios::out | ios::trunc);
+	outputFile.close();
+}
+
+/*
+	vec: vector of floats by reference; the vector to be output
+
+	Return type: string
+*/
+string outputVector(vector<float> &vec) {
+	/*
+	Returns vec's values separated by spaces.
+	*/
+	stringstream s;
+	for (size_t i = 0; i < vec.size()-1; ++i) {
+		s << vec.at(i) << " ";
+	}
+	s << vec.at(vec.size()-1);
+	return s.str();
+}
+
+/*
+	horizVelocityGrid: 2D vector of floats; holds vertical horizontal components at 1/2 indices
+	vertVelocityGrid: 2D vector of floats; holds vertical velocity components at 1/2 indices
+	xDim: int; number of cells in x direction
+	yDim: int; number of cells in y direction
+	fileName: string; the file to output to
+
+	Return type: void
+*/
+void saveVelocityField(vector< vector<float> > &horizVelocityGrid, vector< vector<float> > &vertVelocityGrid, int xDim, int yDim, string fileName) {
+	/*
+	Saves center velocities of given horizontal and vertical velocity fields to fileName.
+	*/
+	vector<float>* centerVelocity;
+	ofstream outputFile;
+	outputFile.open(fileName, ios::app);
+	outputFile << "Start Matrix" << endl;
+	for (size_t i = 0; i < xDim; ++i) {
+		for (size_t j = 0; j < yDim-1; ++j) {
+			centerVelocity = centerVel(horizVelocityGrid, vertVelocityGrid, i, j);
+			outputFile << outputVector(*centerVelocity);
+			outputFile << ";";
+			delete centerVelocity;
+		}
+		centerVelocity = centerVel(horizVelocityGrid, vertVelocityGrid, i, yDim-1);
+		outputFile << outputVector(*centerVelocity);
+		delete centerVelocity;
+		outputFile << endl;
+	}
+	outputFile << "End Matrix" << endl;
+	outputFile.close();
+}
 
 
 /*
